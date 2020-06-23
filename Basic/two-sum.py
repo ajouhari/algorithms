ef77d40d642d5a5143d21e51ab22a89f6e7403b0
Abdulrahman Jouhari <official@ajouhari.com>
2020-06-23 14:25:22 +0300

    Two Sum

--- Basic/two-sum.py
+++ Basic/two-sum.py
@@ -7,9 +7,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i, number in enumerate(nums):
-            compliment = target - number
+        for i, num in enumerate(nums):
+            compliment = target - num
             if compliment not in seen:
-                seen[number] = i
+                seen[num] = i
             else:
                 return [seen[compliment], i]

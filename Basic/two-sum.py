9af52c67273a8d68d075bd510a3ca2d27e6ab2b8
Abdulrahman Jouhari <official@ajouhari.com>
2020-06-23 14:32:29 +0300

    Two Sum

--- Basic/two-sum.py
+++ Basic/two-sum.py
@@ -1,11 +1,12 @@
 # Author: Abdulrahman Jouhari
+# Date: June 19, 2020
 # Project: Two Sum 
 
 # Instructions: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         seen = {}
         for i, number in enumerate(nums):
             compliment = target - number

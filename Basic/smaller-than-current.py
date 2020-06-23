ead1f6ed541d9bc57c8a91e0433cc4c963b0f286
Abdulrahman Jouhari <official@ajouhari.com>
2020-06-23 14:34:17 +0300

    Smaller Than Current

--- Basic/smaller-than-current.py
+++ Basic/smaller-than-current.py
@@ -1,4 +1,5 @@
 # Author: Abdulrahman Jouhari
+# Date: June 21, 2020
 # Project: How Many Numbers Are Smaller Than the Current Number
 
 # Instructions: Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
